9c351c764b5512d54966f3ab81b005ed04b43706
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-12-09 19:14:40 +0545

    bug fixes: search nearest neighbor and cost calculation

--- main.cpp
+++ main.cpp
@@ -52,9 +52,13 @@ private:
     olc::Pixel fill_color = olc::RED; // color to be filled with
     MazeBlock start_block = {0};
     MazeBlock end_block = {0};
-    int nRows = ScreenHeight() / 2;
-    int nCols = ScreenWidth() / 2;
+    // hard coded, pretty bad :(
+    int nRows = 40;
+    int nCols = 40;
 public:
+    Maze(){
+        sAppName = "Path Finder";
+    }
     void DrawBlocks(){
         for (auto block: blocks){
             // kinda bad: It's okay for now
@@ -64,9 +68,9 @@ public:
     }
     // initialize your app
     bool OnUserCreate() override{
-        for(int x = 0 ; x < ScreenWidth() ; x+=10){
-            for(int y = 0 ; y < ScreenHeight() ; y+=10){
-                MazeBlock my_block{y + (x*nRows)/10,x,y,10,10, olc::WHITE};
+        for(int x = 0, addToX = 0 ; x < nCols ; x++, addToX+=10){
+            for(int y = 0, addToY = 0 ; y < nRows ; y++, addToY += 10){
+                MazeBlock my_block{y + (x*nRows),addToX,addToY,10,10, olc::WHITE};
                 blocks.emplace_back(my_block);
             }
         }
@@ -99,7 +103,7 @@ public:
         return true;
     }
     std::vector<int> getNeighbor(MazeBlock b) const{
-       std::vector<int> neighborIndex;
+        std::vector<int> neighborIndex;
         int bottomRight = b.index + nRows + 1;
         int bottomLeft  = b.index - nRows + 1;
         int left = b.index - nRows;
@@ -109,28 +113,28 @@ public:
         int topRight = b.index + nRows - 1;
         int topLeft = b.index - nRows - 1;
 
-        if(bottomRight < 0 || bottomRight > nRows * nCols){}
+        if(bottomRight < 0 || bottomRight >= nRows * nCols || (b.index % nRows == nRows - 1 || b.index > ((nRows - 1) + ((nCols - 2)* nRows)))){}
         else neighborIndex.emplace_back(bottomRight);
 
-        if(bottomLeft < 0 || bottomLeft > nRows * nCols){}
+        if(bottomLeft < 0 || bottomLeft >= nRows * nCols || (b.index % nRows == nRows - 1 || b.index <= nCols - 1)){}
         else neighborIndex.emplace_back(bottomLeft);
 
-        if(left < 0 || left > nRows * nCols){}
+        if(left < 0 || left >= nRows * nCols || b.index % nRows == 0){}
         else neighborIndex.emplace_back(left);
 
-        if(right < 0 || right > nRows * nCols){}
+        if(right < 0 || right >= nRows * nCols || b.index > ((nRows - 1) + ((nCols - 2)* nRows))){}
         else neighborIndex.emplace_back(right);
 
-        if(top < 0 || top > nRows * nCols){}
+        if(top < 0 || top >= nRows * nCols || b.index % nRows == 0){}
         else neighborIndex.emplace_back(top);
 
-        if(bottom < 0 || bottom > nRows * nCols){}
+        if(bottom < 0 || bottom >= nRows * nCols || b.index % nRows == nRows - 1){}
         else neighborIndex.emplace_back(bottom);
 
-        if(topRight < 0 || topRight > nRows * nCols){}
+        if(topRight < 0 || topRight >= nRows * nCols || b.index % nRows == 0 || b.index > ((nRows - 1) + ((nCols - 2)* nRows))){}
         else neighborIndex.emplace_back(topRight);
 
-        if(topLeft < 0 || topLeft > nRows * nCols){}
+        if(topLeft < 0 || topLeft >= nRows * nCols || b.index % nRows == 0 || b.index % nRows == 0){}
         else neighborIndex.emplace_back(topLeft);
 
 
@@ -140,21 +144,27 @@ public:
     MazeBlock searchNearestNeighbor(MazeBlock maze){
         if(maze == end_block) return end_block;
         else{
-            int distanceFromEndX = abs(end_block.startX - maze.startX);
-            int distanceFromEndY = abs(end_block.startY - maze.startY);
 
             std::vector<int> neighbors = getNeighbor(maze);
             MazeBlock nearestBlock = blocks[neighbors[0]];
+
+            int distanceFromEndX = abs(end_block.startX - nearestBlock.startX);
+            int distanceFromEndY = abs(end_block.startY - nearestBlock.startY);
+            int totalCost = distanceFromEndX + distanceFromEndY;
+
             int nearestBlockIndex = neighbors[0];
 
             for(int index: neighbors){
-                if(abs(blocks[index].startX - distanceFromEndX) < abs(nearestBlock.startX - distanceFromEndX)
-                   and abs(blocks[index].startY - distanceFromEndY) < abs(nearestBlock.startY - distanceFromEndY)){
-                    blocks[index].background_color = olc::BLACK;
+                int cost = abs(blocks[index].startX - end_block.startX) + abs(blocks[index].startY - end_block.startY);
+                if(cost <= totalCost && blocks[index].background_color != olc::GREEN && blocks[index].background_color != olc::YELLOW){
                     nearestBlock = blocks[index];
                     nearestBlockIndex = index;
                 }
+                if(blocks[index].background_color != olc::RED && blocks[index].background_color != olc::YELLOW){
+                    blocks[index].background_color = olc::GREEN;
+                }
             }
+            blocks[start_block.index].background_color = olc::RED;
             blocks[nearestBlockIndex].background_color = olc::YELLOW;
             return searchNearestNeighbor(nearestBlock);
         }
